d5fffd5d84e3cee666ee2cd2e83179d456408860
dcodex <dcodex@foxmail.com>
2019-02-07 20:33:38 +0800

    fix auto scan confirm function

--- CookiesPool/run.py
+++ CookiesPool/run.py
@@ -1,8 +1,25 @@
 from CookiesPool.cookiespool.scheduler import Scheduler
+from CookiesPool.cookiespool import importer
+
 
 def main():
-    s = Scheduler()
-    s.run()
+    while True:
+        confirm = input("是否需要导入账号？（Y,n）")
+        if confirm.lower() == 'y' or confirm.lower() == 'n':
+            if confirm == 'y':
+                confirm = True
+            else:
+                confirm = False
+            break
+        print('input error！ try again...')
+
+    if confirm:
+        auto_flag = False if input('是否自动扫描account.txt Y/N: ').lower() == 'n' else auto_flag = True
+        importer.main(auto_Scan=auto_flag)
+    else:
+        s = Scheduler()
+        s.run()
+
 
 if __name__ == '__main__':
     main()
