fa8cb3ff7984979f88bc3fbfe25c9504e9d5d3d0
dcodex <dcodex@foxmail.com>
2019-02-20 14:57:06 +0800

    add parse comment、like、user

--- weibo/spiders/weibo_s.py
+++ weibo/spiders/weibo_s.py
@@ -34,3 +34,10 @@ class WeiboSSpider(Spider):
         url = response.url
         content = ''.join(response.xpath('//div[@id="M_"]//span[@class="ctt"]//text()').extract())
         print(id, url, content)
+        comment_count = response.xpath('//span[@class="pms"]//text()').re_first('评论\[(.*?)\]')
+        forward_count = response.xpath('//a[contains(., "转发[")]//text()').re_first('转发\[(.*?)\]')
+        like_count = response.xpath('//a[contains(., "赞[")]//text()').re_first('赞\[(.*?)\]')
+        print('转发： {} 评论： {} 赞： {}'.format(comment_count, forward_count, like_count))
+        posted_at = response.xpath('//div[@id="M_"]//span[@class="ct"]//text()').extract_first(default=None)
+        user = response.xpath('//div[@id="M_"]/div/a/text()').extract_first(default=None)
+        print(posted_at, user)
